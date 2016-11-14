994045d4182c964fc00a7bbfbfb98cb2407b23d9
Sharon (Siri) Lev <slev@apple.com>
2016-11-13 18:00:02 -0800

    DataSet wrapper

---
+++ src/unittestextras/DataSet.py
@@ -0,0 +1,6 @@
+__author__ = 'Sharon Lev'
+__email__ = 'slev@apple.com'
+__date__ = '11/12/16'
+
+def DataSet(*args):
+  return lambda: args
--- src/unittestextras/__init__.py
+++ src/unittestextras/__init__.py
@@ -2,4 +2,5 @@ __author__ = 'Sharon Lev'
 __email__ = 'sharon_lev@yahoo.com'
 __date__ = '12/30/15'
 from .DataProvider import DataProvider
+from .DataSet import DataSet
 from .DataSource import DataSource
