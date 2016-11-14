52c995069a477fef50238d12cb954b9dd3bd7456
Sharon Lev <sharon_lev@yahoo.com>
2016-11-14 08:29:57 -0800

    added keyed/indexed failure reporting logic

--- src/unittestextras/DataProvider.py
+++ src/unittestextras/DataProvider.py
@@ -3,11 +3,13 @@ __email__ = 'sharon_lev@yahoo.com'
 __date__ = '8/31/15 16:19'
 
 
-def DataProvider(fn_data_provider):
+def DataProvider(fn_data_provider, id_key=None, id_index=None):
   """
   Data provider decorator, allows another callable to provide the data for the test
 
   :param fn_data_provider: an iterable instance that can provide sets of data as expected by the test method decorated
+  :param id_key: optional key to use for reporting failures
+  :param id_index: optional index of argument to use for reporting failures
   """
   def test_decorator(test_method):
     def decorated(self, *args, **kwargs):
@@ -26,10 +28,10 @@ def DataProvider(fn_data_provider):
 
         except AssertionError as AE:
           if hasattr(self, 'logger'): self.logger.error(AE)
-          failures.append(str(explicit_exception(AE, args, counter).message))
+          failures.append(str(explicit_exception(AE, args, id_key, id_index, counter).message))
         except Exception as err:
           if hasattr(self, 'logger'): self.logger.error(err)
-          errors.append((str(explicit_exception(err, args, counter).message)))
+          errors.append((str(explicit_exception(err, args, id_key, id_index, counter).message)))
 
         if (counter < lastrun):
           self.tearDown()
@@ -48,13 +50,18 @@ def DataProvider(fn_data_provider):
     return decorated
   return test_decorator
 
-def explicit_exception(exception, args, id):
+def explicit_exception(exception, args, id_key, id_index, id):
   """
   rename exception message with explicit ID to make it easier to identify failures to data sets
   :param exception: an Exception instance
   :param args: test args set
   :return: updated Exception with explicit message
   """
-  if isinstance(args, dict) and 'DataProviderSetID' in args: id = args['DataProviderSetID']
+  try:
+    id = args[id_key] if id_key else args[id_index] if id_index else id
+  except:
+    id = id
+
+#  if isinstance(args, dict) and 'DataProviderSetID' in args: id = args['DataProviderSetID']
   exception.message = 'Data set [%s]: %s' % (id, exception.message)
   return exception
